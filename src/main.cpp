bb16fc8057174c019da88a044d9d62ab730e3ab3
JJ Kim <jj.dev.001@gmail.com>
2024-08-27 17:52:12 +0900

    Change default shader

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[]) {
   const int width = argc > 1 ? atoi(argv[1]) : 640;
   const int height = argc > 2 ? atoi(argv[2]) : 480;
   const char* title = argc > 3 ? argv[3] : "Shader Renderer";
-  const char* shaderPath = argc > 4 ? argv[4] : "../res/shaders/raytracer.comp";
+  const char* shaderPath = argc > 4 ? argv[4] : "../res/shaders/default.comp";
 
   const App::Config config = {
       .vW = width, .vH = height, .title = title, .shaderPath = shaderPath};
