0416abf8c2d8606e60f2ec1321774b25c93eeaab
JJ Kim <jj.dev.001@gmail.com>
2025-09-01 17:44:40 +0900

    dev: Put the title CLI arg to the last for now

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 int main(int argc, char* argv[]) {
   const int width = argc > 1 ? atoi(argv[1]) : global::DEFAULT_VIEWPORT_WIDTH;
   const int height = argc > 2 ? atoi(argv[2]) : global::DEFAULT_VIEWPORT_HEIGHT;
-  const char* title = argc > 3 ? argv[3] : global::DEFAULT_WINDOW_TITLE;
-  const std::string path = argc > 4 ? argv[4] : "";
+  const std::string path = argc > 3 ? argv[3] : "";
+  const char* title = argc > 4 ? argv[4] : global::DEFAULT_WINDOW_TITLE;
 
   const app::Args args{
       .vW = width,
